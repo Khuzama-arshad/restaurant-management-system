75415a22123dca653e6baf021c4d8f0149130a4b
Khuzama <khuzama003@gmail.com>
2026-02-10 17:27:43 +0500

    Refactor menu to product: update URLs, templates, and models for new product feature

--- management_system/management_system/settings.py
+++ management_system/management_system/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
-    'menu',
+    'product',
     'contact',
     'account',
     'tailwind',
--- management_system/product/models.py
+++ management_system/product/models.py
@@ -1,21 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.Model):
-    MENU_TYPE_LIST=[
+class Product(models.Model):
+    PRODUCT_TYPE_LIST=[
         ('Hot Coffee','Hot Coffee'),
         ('Cold Coffee', 'Cold Coffee'),
         ('Special Coffee', 'Special Coffee'),
     ]
     name = models.CharField(max_length=60)
-    image = models.ImageField(upload_to='menu/')
+    image = models.ImageField(upload_to='product/')
     description = models.TextField(default="")
-    type = models.CharField(max_length=20 , choices=MENU_TYPE_LIST)
+    type = models.CharField(max_length=20 , choices=PRODUCT_TYPE_LIST)
     price = models.DecimalField(
         decimal_places=2,
         max_digits=10,
         default="",
     )
-def __str__(self):
-    return self.name
-
+    def __str__(self):
+        return self.name
